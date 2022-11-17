099dfe4c598017e71fc342648884d7f8e8c5f8ac
SalvaVE <valladares.salvador12@gmail.com>
2022-11-17 09:58:15 -0600

    Cambio 9:58

--- app/src/main/java/com/fjar/transporfast/ui/empresa/loginConductor.java
+++ app/src/main/java/com/fjar/transporfast/ui/empresa/loginConductor.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
@@ -14,6 +15,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.fjar.transporfast.R;
 
 public class loginConductor extends AppCompatActivity {
+
+    private EditText edtcorreo, edtcontrasena;
     private TextView tvMensaje;
     private Button btnRegistrar, btnRecContrasena, btnRecusuario, btnIngresar;
 
@@ -23,11 +26,15 @@ public class loginConductor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        edtcorreo = (EditText) findViewById(R.id.et_usuarioIS);
+        edtcontrasena = (EditText) findViewById(R.id.et_contrasenaIS);
         btnRegistrar = (Button) findViewById(R.id.btnRegistrarse);
         tvMensaje = (TextView) findViewById(R.id.mensajeRegistrar);
         btnRecContrasena = (Button) findViewById(R.id.btnRecuperarContrasena);
         btnRecusuario = (Button) findViewById(R.id.btnRecuperarUsuario);
-        btnIngresar = (Button) findViewById(R.id.btnIngresar);
+
+
+        edtcorreo.setVisibility(View.GONE);
 
         btnRegistrar.setVisibility(View.GONE);
         tvMensaje.setVisibility(View.GONE);
