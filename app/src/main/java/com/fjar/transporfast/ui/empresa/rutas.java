e2443d6e7f2542b61505200c3fda98ecd512215c
Mor89 <100388526+Mor89@users.noreply.github.com>
2022-11-16 22:51:22 -0600

    nuevo

--- app/src/main/java/com/fjar/transporfast/ui/empresa/rutas.java
+++ app/src/main/java/com/fjar/transporfast/ui/empresa/rutas.java
@@ -1,27 +1,50 @@
 package com.fjar.transporfast.ui.empresa;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.fjar.transporfast.MainActivity;
+import com.fjar.transporfast.MySingleton;
 import com.fjar.transporfast.R;
+import com.fjar.transporfast.ui.RutaDTO;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class rutas extends Fragment {
 
     TextView tvRutas;
+    EditText etRutas;
+    Button btnGuardar, btnEliminar, btnModificar;
     boolean [] selectedRuta;
     ArrayList<Integer> rutaList = new ArrayList<>();
     String[] rutaArray = {"3LP", "4LP", "5LP"};
@@ -125,6 +148,72 @@ public class rutas extends Fragment {
             }
         });
 
+        btnGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = etRutas.getText().toString();
+                String spinner = tvRutas.getText().toString();
+
+
+                if(id.length() == 0){
+                    etRutas.setError("Campo obligatotio");
+                }else if (spinner.length() == 0){
+                    tvRutas.setError("Campo obligatorio");
+                }
+            }
+        });
         return root;
+    }
+
+    //Método para obtener todos los productos
+    public void obtenerUsuariosLista (final Context context, ListView lv) {
+        //Creamos un array en el cual guardaremos cada una de los datos que vendran de nuestra API
+        ArrayList<String> usuarios = new ArrayList<String>();
+
+        String url = "https://franciscowebtw.000webhostapp.com/service2020/obtenerUsuarios.php";
+        StringRequest request = new StringRequest(Request.Method.POST,url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+
+                    JSONArray peticionJSON = new JSONArray(response.toString());
+                    usuarios.add("NOMBRE - ID ");
+                    for (int i = 0; i < peticionJSON.length(); i ++){
+                        JSONObject requestJSON = peticionJSON.getJSONObject(i);
+                        RutaDTO usuariotmp = new RutaDTO();
+                        String nombre = requestJSON.getString("nombre");
+                        String Id = requestJSON.getString("Id");
+                        Log.e("guarda daatos", "si");
+                        //Añadimos los datos a nuestro objeto usuario
+                        usuariotmp.setNombre(nombre);
+                        usuariotmp.setId(Id);
+
+
+                        usuarios.add(usuariotmp.getNombre() + " - " + usuariotmp.getId());
+
+                        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, usuarios);
+                        lv.setAdapter(adaptador);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Toast.makeText(context, "No se pudo obtener los usuarios \n" +"Intentelo más tarde.", Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //En este método se colocan o se setean los valores a recibir por el fichero *.php
+                Map<String, String> map = new HashMap<>();
+                map.put("Content-Type", "application/json; charset=utf-8");
+                map.put("Accept", "application/json");
+                return map;
+            }
+        };
+        MySingleton.getInstance(context).addToRequestQueue(request);
+
     }
 }
